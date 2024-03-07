fa5c096412df9d13156986ba18e8f019ed7ff81d
alanromo999 <68311832+alanromo999@users.noreply.github.com>
2024-03-07 14:13:54 -0600

    Add files via upload

---
+++ Show_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  char loop;
+
+    puts("Showing the alphabet:");
+        for(loop='a';loop<='z';loop++)
+        putchar(loop);
+
+  return 0;
+}
